d799c934b14fb15a0f80f6cc91b4c8c8a5674e86
BryanHo10 <bryankho1010@gmail.com>
2019-11-01 14:13:01 -0700

    Hide Token; Display private repos

--- src/components/Repositories/repo-view.js
+++ src/components/Repositories/repo-view.js
@@ -1,14 +1,12 @@
 import React from "react"
 
 /* PROPS : 
-        "id": 156,
-        "date": "2018-11-18",
-        "title": "A True Servant and a False Kin",
-        "series": "Mark",
-        "series_num": 17,
-        "passage": "Mark 6:14-29",
-        "speaker": "Pastor Ryan Cheung",
-        "audio_link": "http://www.cbcoc.org/content/media/audio/englishworship/english_2018_11_18_cheung.mp3"
+		createDate = created_at: "2018-09-07T18:56:54Z"
+		gitUrl = clone_url: "https://github.com/ilxl-ppr/restaurant-bill.git"
+		id: 147863236
+		language: "C++"
+		name: "restaurant-bill"
+
 */
 
 class RepoView extends React.Component {
@@ -19,40 +17,22 @@ class RepoView extends React.Component {
 		};
 	}
 	render(){
-		let dateString = new Date(this.props.date).toDateString();
+		let dateString = new Date(this.props.created_at).toDateString();
+		
 		let dateValues = dateString.split(' '); // [Wed, Jul, 28, 1993]
 		let month = dateValues[1];
 		let day = dateValues[2];
 		let year = dateValues[3];
 
-
 		return(
 			<div className="container-fluid row py-3">
-				<div className="col-lg-2 text-center">
-					<h5 className="gotham-bold">{year}</h5>	
-					<h1 className="gotham-bold">{month}<br/>{day}</h1>	
-				</div> 
 				<div className="col-lg">
-					<h2 id="message-title" className="gotham-med">{this.props.title}</h2>
-					<h5 className="gotham-book pb-4">{this.props.speaker}<br/>{this.props.passage}</h5>
-
-					<audio controls className="container-fluid">
-						<source src={this.props.audio_link} />
-					</audio>	
-				</div>
-				<div className="col-lg-2">
-					<div>
-						<a download="message" href={this.props.audio_link}>
-							<h6 className="fa-solid" id="attach-icon"></h6>
-							<h6 id="attach-icon" className="gotham-book">  Download</h6>
-						</a>
-					</div>
-					<div>
-						<a href={this.props.audio_link}>
-							<h6 className="fa-solid" id="attach-icon"></h6>
-							<h6 id="attach-icon" className="gotham-book">  Link</h6>
-						</a>
-					</div>
+					<h5 id="message-title" className="gotham-med">{month} {day}, {year}</h5>
+					<h2 id="message-title" className="gotham-med">{this.props.name}</h2>
+					<h6 className="gotham-book pb-4">{this.props.language}</h6>	
+					<a download="message" href={this.props.gitUrl}>
+						<h6 id="attach-icon" className="gotham-book">  Github Link</h6>
+					</a>
 				</div>  
 			</div>
 		);
--- src/components/body.js
+++ src/components/body.js
@@ -2,6 +2,8 @@ import React from "react"
 import {withPrefix} from "gatsby"
 import * as $ from "jquery" 
 import { element } from "prop-types";
+import RepoView from "./Repositories/repo-view"
+import access_token from  "../data/github-ppr.json"
 
 class BodyView extends React.Component {
   //Probably should take a MainObject instead of individual props.
@@ -10,31 +12,40 @@ class BodyView extends React.Component {
 
     this.state={
       uri:"img/" + this.props.image,
-      token:"",
-      externalData: null,
+      token: access_token["token"],
+      repoData: null,
+
     };
   }
   componentDidMount() {
 
       $.get(`https://api.github.com/orgs/${"ilxl-ppr"}/repos?access_token=${this.state.token}`).done(function(data) {
-      this.setState({externalData: data});
+      this.setState({repoData: data});
     }.bind(this));
 
   }
   render() {
       
-    console.log(this.state.externalData);
-    if(this.state.externalData){
-        console.log(this.state.externalData);
+    console.log(this.state.repoData);
+    if(this.state.repoData){
+        console.log(this.state.repoData);
         return (
         <div id="jumbo-container">
-            {this.state.externalData.map((elem)=>{
-                return (<div key={elem.name}>{elem.name}</div>);
+            {this.state.repoData.map((elem)=>{
+                return (
+                <RepoView 
+                    key={elem.id}
+                    created_at= {elem.created_at}
+                    gitUrl = {elem.clone_url}
+                    language= {elem.language}
+                    name=  {elem.name}
+                />);
             })}
         </div>
         );
     }
     else{
+        this.retrieveGithubRepo("ilxl-ppr");
         return (
             <div id="jumbo-container">
             </div>
@@ -42,15 +53,9 @@ class BodyView extends React.Component {
     }
   }
   retrieveGithubRepo(organization){
-    $.ajax({
-        url: `https://api.github.com/orgs/${organization}/repos?access_token=${this.state.token}`,
-        data:{
-
-        },
-        complete: function( result ) {
-            console.log(result.responseJSON);
-        }
-      });
+    $.get(`https://api.github.com/orgs/${"ilxl-ppr"}/repos?access_token=${this.state.token}`).done(function(data) {
+      this.setState({repoData: data});
+    }.bind(this));
   }
 }
 export default BodyView;
